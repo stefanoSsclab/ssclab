03874667959d63cde94d8a47965eddbf58d4b505
Stefano Scarioli <stefano.scarioli@iet.it>
2024-01-02 13:07:51 +0100

    starano

--- src/xample/prova.java
+++ src/xample/prova.java
@@ -6,7 +6,7 @@ public class prova {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub perv
+		// TODO Auto-generated method stub perv n
 		double d=-3.999999999;
 		System.out.println("-->D0:"+(Math.floor(d) ));
 
