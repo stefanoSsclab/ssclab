917213f8c12fe6eb4db03b5c5368fde0437c2d83
Stefano Scarioli <stefano.scarioli@iet.it>
2024-01-02 12:53:54 +0100

    commit da eclipse

--- src/xample/prova.java
+++ src/xample/prova.java
@@ -6,7 +6,7 @@ public class prova {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub perv
 		double d=-3.999999999;
 		System.out.println("-->D0:"+(Math.floor(d) ));
 
