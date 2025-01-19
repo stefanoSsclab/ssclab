5f700de3e8800f507c23c96cd024b04bb2fc7011
stefano scarioli <info@ssclab.org>
2025-01-19 14:11:12 +0100

    4.7.6 Miglioramento controlli se sbagliata sintassi bound con virgole

--- src/org/ssclab/pl/milp/util/CsvSplitter.java
+++ src/org/ssclab/pl/milp/util/CsvSplitter.java
@@ -15,7 +15,7 @@ public class CsvSplitter {
 	                "      3Y + X2   + Z      +5X5  = 12 \n"+
 	                "      6Y +3.0x2 +4Z +5X4      <= 124\n"+
 	                "       Y +3x2       +3X4 +6X5 <= 854\n"+
-	                "-1<=  x2 <= 6, Y >=0 , N >=0\n"+
+	                "-1<=  x2 <= 6, Y >=0 ,  N >=0\n"+
 	                ". <=  z  <= . , x4 >=5 \n"+
 	                "int x2,x3,x4";
 
@@ -37,7 +37,10 @@ public class CsvSplitter {
 	public static BufferedReader splitterBounds(BufferedReader originalReader) throws IOException,ParseException {
     	
 		StringWriter stringWriter = new StringWriter() ;
-        String line;
+		Pattern pattern_min =Pattern.compile("\\s*(((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.))\\s*<\\s*=)?\\s*(\\p{Alpha}+\\w*)\\s*(<\\s*=\\s*((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.)))?\\s*");
+		Pattern pattern_max =Pattern.compile("\\s*(((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.))\\s*>\\s*=)?\\s*(\\p{Alpha}+\\w*)\\s*(>\\s*=\\s*((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.)))?\\s*");
+		Matcher matcher_min,matcher_max;
+		String line;
         String[] tokens;
     	while ((line = originalReader.readLine()) != null) {
     		if(line.matches("(.+)((<\\s*=)|(>\\s*=)|(=))(.+)")) { 
@@ -46,15 +49,28 @@ public class CsvSplitter {
 	            for (String token : tokens) {
 	                // Scrive ogni token come una nuova riga nello StringWriter
 	            	if(tokens.length > 1 ) {
-	            		//System.out.println("entrato"); // Stampa ogni nuova riga
-		            	if (token.matches("\\s*(((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.))\\s*<\\s*=)?\\s*(\\p{Alpha}+\\w*)\\s*(<\\s*=\\s*((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.)))?\\s*")) {
+	            		
+	            		matcher_min = pattern_min.matcher(token);
+	            		matcher_max = pattern_max.matcher(token);
+	            		
+	            		//System.out.println("entrato:"+line); // Stampa ogni nuova riga
+		            	if (matcher_min.matches()) {
 		        			//non sono presenti tutti i controlli, altri controlli li fa quando lo elabora
+		            		//System.out.println("MIN:"+token);
+		            		if(matcher_min.group(1)==null && matcher_min.group(9)==null) {
+		            			 //System.out.println("ERRORE");
+		            			 throw new ParseException(RB.getString("it.ssc.pl.milp.ScanConstraintFromString.msg1")+" "+line+"");
+		            		}
 		        		}
 		        		//pattern per identificare uppur o lower del tipo  "nome_upper: a>= x >= b"  , anche con a o b mancanti 
-		        		else if (token.matches("\\s*(((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.))\\s*>\\s*=)?\\s*(\\p{Alpha}+\\w*)\\s*(>\\s*=\\s*((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.)))?\\s*")) {
+		        		else if (matcher_max.matches()) {
 		        			//non sono presenti tutti i controlli, altri controlli li fa quando lo elabora
-		        		}
-		            			            	
+		        			//System.out.println("MAX:"+token);
+		        			if(matcher_max.group(1)==null && matcher_max.group(9)==null) {
+		            			 //System.out.println("ERRORE");
+		            			 throw new ParseException(RB.getString("it.ssc.pl.milp.ScanConstraintFromString.msg1")+" "+line+"");
+		            		}
+		        		}	            	
 		        		else {
 		        			throw new ParseException(RB.getString("it.ssc.pl.milp.ScanConstraintFromString.msg1")+" "+line+"");
 		        		}
@@ -71,23 +87,19 @@ public class CsvSplitter {
 	
     }
 	
-	
-	
-	
-	
-	
-	
-public static BufferedReader splitterBoundsAndDouble(BufferedReader originalReader) throws IOException,ParseException {
+
+    public static BufferedReader splitterBoundsAndDouble(BufferedReader originalReader) throws IOException,ParseException {
     	
 		StringWriter stringWriter = new StringWriter() ;
+		Pattern pattern_double =Pattern.compile("\\s*(\\p{Alpha}+\\w*\\s*:)?(.+)((<|>)\\s*=)(.+)((<|>)\\s*=)(.+)");
         String line;
         String[] tokens;
+        Matcher matcher_double;
     	while ((line = originalReader.readLine()) != null) {
     		if(line.matches("\\s*(\\p{Alpha}+\\w*\\s*:)?(.+)(<\\s*=)(\\s*([+-]?)\\s*(((\\d+)(\\.)?(\\d*))|(\\[(.+?)\\]))?((\\p{Alpha}+)(\\w*))\\s*){2,}(<\\s*=)(.+)") || 
     		   line.matches("\\s*(\\p{Alpha}+\\w*\\s*:)?(.+)(>\\s*=)(\\s*([+-]?)\\s*(((\\d+)(\\.)?(\\d*))|(\\[(.+?)\\]))?((\\p{Alpha}+)(\\w*))\\s*){2,}(>\\s*=)(.+)")		         ) { 
     			
-    			Pattern pattern_double =Pattern.compile("\\s*(\\p{Alpha}+\\w*\\s*:)?(.+)((<|>)\\s*=)(.+)((<|>)\\s*=)(.+)");
-    			Matcher matcher_double = pattern_double.matcher(line);
+    			matcher_double = pattern_double.matcher(line);
     			if (matcher_double.matches()) { 
     			//System.out.println("numero--:"+matcher_double.groupCount());
     			
@@ -124,33 +136,13 @@ public static BufferedReader splitterBoundsAndDouble(BufferedReader originalRead
     			// Suddivide la riga in token separati da virgole
 	            tokens = line.split(",");
 	            for (String token : tokens) {
-	                // Scrive ogni token come una nuova riga nello StringWriter
-	            	if(tokens.length > 1 ) {
-	            		//System.out.println("entrato"); // Stampa ogni nuova riga
-		            	if (token.matches("\\s*(((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.))\\s*<\\s*=)?\\s*(\\p{Alpha}+\\w*)\\s*(<\\s*=\\s*((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.)))?\\s*")) {
-		        			//non sono presenti tutti i controlli, altri controlli li fa quando lo elabora
-		        		}
-		        		//pattern per identificare uppur o lower del tipo  "nome_upper: a>= x >= b"  , anche con a o b mancanti 
-		        		else if (token.matches("\\s*(((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.))\\s*>\\s*=)?\\s*(\\p{Alpha}+\\w*)\\s*(>\\s*=\\s*((([+-]?)\\s*(\\d+\\.?\\d*|\\[(.+?)\\]))|(\\.)))?\\s*")) {
-		        			//non sono presenti tutti i controlli, altri controlli li fa quando lo elabora
-		        		}
-		            			            	
-		        		else {
-		        			throw new ParseException(RB.getString("it.ssc.pl.milp.ScanConstraintFromString.msg1")+" "+line+"");
-		        		}
-	            	}	
 	                stringWriter.write(token+System.lineSeparator());
 	            }
     		}
     		else  stringWriter.write(line+System.lineSeparator());
         }
-
        originalReader.close();
        //Converte lo StringWriter in un nuovo BufferedReader
        return new BufferedReader(new StringReader(stringWriter.toString())); 
-	
     }
-	
-	
-	
 }
